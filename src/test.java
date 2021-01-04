40808ffa3386f045c264e07eef98f4589c1d8600
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-04 16:10:24 +0200

    Muutujad ja andmetüübid - ülesanne 1.2

--- src/test.java
+++ src/test.java
@@ -1,9 +1,17 @@
+/*
+ * Muutujate kasutamise näide
+ * autor - Gardo Kirsipuu
+ * ülesanne 1.2
+ * */
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("Täna õppisime:");
-        System.out.println("1. JDK installeerimine ja seadistamine");
-        System.out.println("2. IDE installeerimine ja seadistamine");
-        System.out.println("3. IDE kasutamine");
-        System.out.println("4. git VCS kasutamine");
+        // defineerime muutujad
+        int aasta = 2020;
+        String liblikas = "teelehe-mosaiikliblikas";
+        String lauseKeskosa = ". aasta liblikas on ";
+        String lause = aasta + lauseKeskosa + liblikas;
+        // völjastamine
+        System.out.println(lause);
     }
 }
