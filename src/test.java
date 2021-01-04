b332f1fb320a504ede49e6631a3cfbdc209e5d25
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-04 20:42:10 +0200

    ülesanne 2.3b

--- src/test.java
+++ src/test.java
@@ -1,22 +1,35 @@
 /*
  * autor - Gardo Kirsipuu
- * ülesanne 1.4a
+ * ülesanne 2.3b
  * */
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
         // defineerime muutujad
+        Random rand = new Random();
         // lubame kasutajale sisestada midagi
         Scanner sisend = new Scanner(System.in);
         // selgitame kasutajale mida tuleb sisestada
-        System.out.println("Sisestage ainepunktide arv: ");
-        int ainepunktideArv = sisend.nextInt();
-        System.out.println("Sisestage nadalate arv: ");
-        int nadalateArv = sisend.nextInt();
-        int ajakulu = ainepunktideArv * 26;
-        double nadalaAjakulu = (double) ajakulu / nadalateArv;
-        System.out.println(Math.round(nadalaAjakulu));
+        System.out.println("Kas soovite istekohta ise valida (ise) voi loosida (loos)? ");
+        String iseLoos = sisend.next();
+        if (iseLoos.equals("loos")) {
+            int rand_int = rand.nextInt(3);
+            if (rand_int <= 1) {
+                System.out.println("Istekoht loositi. Vahekaigukoht");
+            } else if (rand_int > 1) {
+                System.out.println("Istekoht loositi. Aknakoht");
+            }
+        } else if (iseLoos.equals("ise")) {
+            System.out.println("Kas soovite istuda akna aares (aken) voi mitte (muu)");
+            String ise = sisend.next();
+            if (ise.equals("aken")) {
+                System.out.println("Valisite ise. Aknakoht");
+            } else if (ise.equals("muu")) {
+                System.out.println("Valisite ise. Vahekaigukoht");
+            }
+        }
     }
 }
