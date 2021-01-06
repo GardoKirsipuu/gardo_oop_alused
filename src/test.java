6a6e22060a048e1c8d826d7e027bce28c01c149e
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-06 16:32:51 +0200

    tsüklid - ülesanne 3.2

--- src/test.java
+++ src/test.java
@@ -17,17 +17,21 @@ public class test {
         // lubame kasutajale sisestada midagi
         Scanner sisend = new Scanner(System.in);
         // selgitame kasutajale mida tuleb sisestada
-        System.out.println("Täringute arv: ");
+        System.out.println("Sisesta ringide arv: ");
         // salvestame kasutaja sisestatud väärtuse
-        int taringuteArv = sisend.nextInt();
+        int ringideArv = sisend.nextInt();
+        int joostud = 0;
+        int porgandid = 0;
+        int mitu = 2;
         // korduv tegevus
         // kontroll
-        while (true) {
-            int taring = (int) Math.round(Math.random() * 6);
-            System.out.println(taring);
-            if (taring == taringuteArv) {
-                break;
+        while (joostud < ringideArv) {
+            if (joostud % 2 == 0) {
+                porgandid += mitu;
+                mitu += 2;
             }
+            joostud++;
         }
+        System.out.println(porgandid);
     }
 }
