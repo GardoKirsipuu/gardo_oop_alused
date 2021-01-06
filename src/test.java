0ae6604d019ce2c4181a09a4ffb7e1aea89abd57
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-06 15:40:00 +0200

    tsüklid - ülesanne 3.1

--- src/test.java
+++ src/test.java
@@ -3,33 +3,31 @@
  * ülesanne 2.3b
  * */
 
-import java.util.Random;
+/*
+ * while tsükkel
+ * autor Gardo Kirsipuu
+ * Ülesanne 3.1
+ */
+
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
         // defineerime muutujad
-        Random rand = new Random();
         // lubame kasutajale sisestada midagi
         Scanner sisend = new Scanner(System.in);
         // selgitame kasutajale mida tuleb sisestada
-        System.out.println("Kas soovite istekohta ise valida (ise) voi loosida (loos)? ");
-        String iseLoos = sisend.next();
-        if (iseLoos.equals("loos")) {
-            int rand_int = rand.nextInt(3);
-            if (rand_int <= 1) {
-                System.out.println("Istekoht loositi. Vahekaigukoht");
-            } else if (rand_int > 1) {
-                System.out.println("Istekoht loositi. Aknakoht");
-            }
-        } else if (iseLoos.equals("ise")) {
-            System.out.println("Kas soovite istuda akna aares (aken) voi mitte (muu)");
-            String ise = sisend.next();
-            if (ise.equals("aken")) {
-                System.out.println("Valisite ise. Aknakoht");
-            } else if (ise.equals("muu")) {
-                System.out.println("Valisite ise. Vahekaigukoht");
-            }
+        System.out.println("Sisesta mitu korda tuleb äratada: ");
+        // salvestame kasutaja sisestatud väärtuse
+        int mituKorda = sisend.nextInt();
+        // korduv tegevus
+        // defineerime tsükli juhtimismuutuja
+        int kord = 1;
+        // kontroll
+        while (kord <= mituKorda) {
+            System.out.println("Tõuse ja sära!");
+            // juhtimismuutuja väärtuse suurendamine
+            kord++; // sama, mis kord += 1
         }
     }
 }
