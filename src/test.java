f6a85370d2f3e2d79ed67dde222c44f48d874285
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-09 02:51:16 +0200

    meetodid - ülesanne 6.1

--- src/test.java
+++ src/test.java
@@ -4,31 +4,39 @@
  * */
 
 /*
- * while tsükkel
+ * meetod + return - näide
  * autor Gardo Kirsipuu
- * Ülesanne 3.4a
+ * Ülesanne 6.1
  */
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class test {
+    // banner meetod
+    static String banner(String reklaamlause) {
+        String reklaamlauseSuurteTahtedega = reklaamlause.toUpperCase();
+        return reklaamlauseSuurteTahtedega;
+    }
+
+    // main meetod
     public static void main(String[] args) {
         // defineerime muutujad
         // lubame kasutajale sisestada midagi
         Scanner sisend = new Scanner(System.in);
         // selgitame kasutajale mida tuleb sisestada
-        System.out.println("Sisesta ringide arv: ");
+        System.out.println("Mitu korda kuvada reklaamlauset?: ");
         // salvestame kasutaja sisestatud väärtuse
-        int ringideArv = sisend.nextInt();
-        int i = 0;
-        int porgandid = 0;
-        // korduv tegevus
-        // kontroll
-        while (ringideArv > i) {
-            i++;
-            porgandid += i * (i + 1) / 2;
+        int kordamine = sisend.nextInt();
+        // sammuti sisendid
+        sisend = new Scanner(System.in);
+        System.out.println("Sisesta reklaamlause");
+        String lause = sisend.nextLine();
+        // kordamine
+        int kord = 1;
+        while (kord <= kordamine) {
+            System.out.println(banner(lause));
+            kord++;
         }
-        System.out.print("porgandite koguarv on ");
-        System.out.print(porgandid);
     }
 }
