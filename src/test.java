1414282496ef31edf6a429ead6cdddad41925e3e
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-11 16:38:45 +0200

    Massiivid - ülesanne 5.4a

--- src/test.java
+++ src/test.java
@@ -6,7 +6,7 @@
 /*
  * massiivid
  * autor Gardo Kirsipuu
- * ülesanne 5.3
+ * ülesanne 5.4a
  */
 
 import java.io.File;
@@ -16,9 +16,14 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         // massiiv faili sisu hoidmiseks
-        ArrayList<Double> palk = new ArrayList<>();
+        ArrayList<String> palk = new ArrayList<>();
+        ArrayList<String> laulud = new ArrayList<>();
+        // faili nime küsimine
+        Scanner sisend = new Scanner(System.in);
+        System.out.println("Sisestage faili nimi: ");
+        String failiNimi = sisend.nextLine();
         // määrame fail ja kontrollime, kas on võiamlik lugeda andmed
-        File fail = new File("C:\\Users\\gardo\\IdeaProjects\\oop_alused\\src\\konto.txt");
+        File fail = new File("C:\\Users\\gardo\\IdeaProjects\\oop_alused\\src\\" + failiNimi);
         Scanner sisendFailist = null;
         try {
             sisendFailist = new Scanner(fail);
@@ -27,16 +32,20 @@ public class test {
         }
         // loeme failist
         while (sisendFailist.hasNextLine()) {
-            Double rida = sisendFailist.nextDouble();
+            String rida = sisendFailist.nextLine();
             palk.add(rida); // lisame loetud väärtus nimekirja sisse
         }
         sisendFailist.close();
+        // järjekord
+        int jarjekord = 1;
         // vaatame nimekirja sisu
         for (int i = 0; i < palk.size(); i++) {
-            if (palk.get(i) > 0.0) {
-                System.out.println(palk.get(i));
-            }
+            System.out.println(jarjekord + ". " + palk.get(i));
+            jarjekord++;
         }
+        System.out.println("Mis laulu soovid?: ");
+        int lauluNimi = sisend.nextInt();
+        System.out.println(palk.get(lauluNimi - 1));
+    }
 
     }
-}
