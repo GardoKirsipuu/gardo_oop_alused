2eb32c855235e490df446b6c3e59046e6ef1f827
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-11 14:54:46 +0200

    Massiivid - [] ja {} abil

--- src/test.java
+++ src/test.java
@@ -4,36 +4,21 @@
  * */
 
 /*
- * meetod + return - näide
+ * massiivid
  * autor Gardo Kirsipuu
- * Ülesanne 6.4a
+ *
  */
 
-import java.util.Scanner;
+
 
 public class test {
-    // tervitus meetod
-    static Object tervitus(int kulalisteArv) {
-        System.out.println("Võõrustaja: \"Tere!\"");
-        System.out.println("Täna " + kulalisteArv + ". korda tervitada, mõtistkleb võõrustaja.");
-        System.out.println("Külaline:  \"Tere, suur tänu kutse eest!\"");
-        return null;
-    }
-
-    // main meetod
     public static void main(String[] args) {
-        // defineerime muutujad
-        // lubame kasutajale sisestada midagi
-        Scanner sisend = new Scanner(System.in);
-        // selgitame kasutajale mida tuleb sisestada
-        System.out.println("Sisestage külaliste arv: ");
-        // salvestame kasutaja sisestatud väärtuse
-        int kulalisteArv = sisend.nextInt();
-        int kord = 1;
-        // tsükkel
-        while (kulalisteArv >= kord) {
-            tervitus(kord);
-            kord++;
-        }
+        int[] numbrid = {1, 2, 3, 4, 5};
+        System.out.println(numbrid.length);
+        System.out.println(numbrid[0]);
+        System.out.println(numbrid[1]);
+        System.out.println(numbrid[2]);
+        System.out.println(numbrid[3]);
+        System.out.println(numbrid[4]);
     }
 }
