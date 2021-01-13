2e492ef661674ab877b01d5d38dc373361a2c54c
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-13 19:03:04 +0200

    massiivid - ülesanne 5.4c

--- src/test.java
+++ src/test.java
@@ -11,11 +11,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
+    // pronksikarvaSumma meetod
+    static Object pronksikarvaSumma(int[] mundid) {
+        int summa = 0;
+        for (int i = 0; i < mundid.length; i++) {
+            if (i <= 5) {
+                summa += i;
+            }
+        }
+        return summa;
+    }
+
     public static void main(String[] args) {
         // massiiv faili sisu hoidmiseks
-        ArrayList<String> opilased = new ArrayList<>();
+        ArrayList<Integer> mundid = new ArrayList<>();
+        // küsime millist faili avada
+        Scanner sisend = new Scanner(System.in);
+        System.out.println("Sisesta failinimi: ");
+        String failiNimi = sisend.nextLine();
         // määrame fail ja kontrollime, kas on võiamlik lugeda andmed
-        File fail = new File("C:\\Users\\gardo\\IdeaProjects\\oop_alused\\src\\nimekiri.txt");
+        File fail = new File("C:\\Users\\gardo\\IdeaProjects\\oop_alused\\src\\" + failiNimi);
         Scanner sisendFailist = null;
         try {
             sisendFailist = new Scanner(fail);
@@ -23,25 +38,12 @@ public class test {
             System.out.println("Faili pole - " + e.getMessage());
         }
         // loeme failist
-        while (sisendFailist.hasNextLine()) {
-            String rida = sisendFailist.nextLine();
-            opilased.add(rida); // lisame loetud väärtus nimekirja sisse
+        while (sisendFailist.hasNextInt()) {
+            int rida = sisendFailist.nextInt();
+            mundid.add(rida); // lisame loetud väärtus nimekirja sisse
         }
         sisendFailist.close();
-        // loome tänase kuupäeva
-        LocalDateTime tananeKuupaev = LocalDateTime.now();
-        System.out.println("Tänane kuupäev ilma vorminduseta " + tananeKuupaev);
-        DateTimeFormatter kuupaevaVormindus = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:MM:ss");
-        String tananeKuupaevVormindatud = tananeKuupaev.format(kuupaevaVormindus);
-        System.out.println("Tänane vormindatud kuupäev " + tananeKuupaevVormindatud);
-        // otsime vajaliku inimese nimekirjast
-        int indeks = Integer.parseInt(tananeKuupaevVormindatud);
-        System.out.println(opilased.get(indeks - 1));
-        // vaatame nimekirja sisu
-        /*
-        for (int i = 0; i < opilased.size(); i++) {
-            System.out.println(i + " " + opilased.get(i));
-        }
-        */
+        // väljastame müntide summa
+        System.out.println("Hoiupõrsasse läheb " + pronksikarvaSumma(mundid) + " senti");
     }
 }
