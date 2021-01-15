0dc8dce8f69255c1f61c99b7e9cf8efafc9693a4
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-15 13:25:05 +0200

    HashMap

--- src/test.java
+++ src/test.java
@@ -1,49 +1,36 @@
 /*
- * massiivid
  * autor Gardo Kirsipuu
- * ülesanne 5.4c
+ 1. Luua uusi kontosid, andes kontole mingi hulga raha
+ 2. Kanda kontole raha
+    a) Kontole ei saa kanda negatiivset summat
+ 3. Võtta kontolt raha
+    a) Kontolt ei saa võtta negatiivset summat
+    b) Kontolt ei saa võtta rohkem raha kui kontol on
+ 4. Kanda ühelt kontolt raha teisele
+    a) Kehtivad samad reeglid, mis kontole raha kandmisel ja välja võtmisel
+
+ Iga tegevus peab võimalusel olema vormistatud eraldi meetodina
+ Programm peab olema piisavalt dokumenteeritud
  */
 
-import java.io.File;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class test {
-    // pronksikarvaSumma meetod
-    static Object pronksikarvaSumma(int[] mundid) {
-        int summa = 0;
-        for (int i = 0; i < mundid.length; i++) {
-            if (i <= 5) {
-                summa += i;
-            }
-        }
-        return summa;
-    }
-
     public static void main(String[] args) {
-        // massiiv faili sisu hoidmiseks
-        ArrayList<Integer> mundid = new ArrayList<>();
-        // küsime millist faili avada
+        HashMap<String, String> grupp = new HashMap<String, String>();
+        grupp.put("matti", "mage");
+        grupp.put("mikael", "miku");
+        grupp.put("arto", "arppa");
+        /*
+        for (String nimi: grupp.keySet()) {
+            System.out.println(nimi + " - " + grupp.get(nimi));
+        }*/
         Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisesta failinimi: ");
-        String failiNimi = sisend.nextLine();
-        // määrame fail ja kontrollime, kas on võiamlik lugeda andmed
-        File fail = new File("C:\\Users\\gardo\\IdeaProjects\\oop_alused\\src\\" + failiNimi);
-        Scanner sisendFailist = null;
-        try {
-            sisendFailist = new Scanner(fail);
-        } catch (Exception e) {
-            System.out.println("Faili pole - " + e.getMessage());
-        }
-        // loeme failist
-        while (sisendFailist.hasNextInt()) {
-            int rida = sisendFailist.nextInt();
-            mundid.add(rida); // lisame loetud väärtus nimekirja sisse
-        }
-        sisendFailist.close();
-        // väljastame müntide summa
-        System.out.println("Hoiupõrsasse läheb " + pronksikarvaSumma(mundid) + " senti");
+        System.out.println("Sisesta nimi: ");
+        String nimi = sisend.nextLine();
+        nimi = nimi.toLowerCase(Locale.ROOT);
+        System.out.println(nimi + " - " + grupp.get(nimi));
     }
 }
