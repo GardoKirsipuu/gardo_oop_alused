2be66e07e8edca8971dae42f982ec9b83782c810
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-01-29 09:59:58 +0200

    Polümorphismi näide - 29.01

--- src/Inimene.java
+++ src/Inimene.java
@@ -37,4 +37,32 @@ public class Inimene {
         return this.mass / (this.pikkus * this.pikkus);
     }
 
+    public void suurendaKaal() {
+        this.mass++;
+    }
+
+    public void suurendaKaal(double kg) {
+        this.mass += kg;
+    }
+
+    public void vahendaKaal() {
+        this.mass--;
+    }
+
+    public void nomraalKaal() {
+        // kui kmi on 19-24,9 - see on norm
+        // kui kmi on alla 19, tuleb inimese kaalu suurendada
+        // iga kord 1 kg juurde ja kontrollida
+        if (this.kmi() >= 19.0 && this.kmi() <= 24.9) {
+            System.out.println("Kaal on normis");
+        } else {
+            while (this.kmi() < 19.0) {
+                this.suurendaKaal();
+            }
+            while (this.kmi() > 24.9) {
+                this.vahendaKaal();
+            }
+        }
+    }
+
 }
--- src/test.java
+++ src/test.java
@@ -7,17 +7,18 @@ Kapseldus, getterid, setterid ja private
 public class test {
     public static void main(String[] args) {
         Inimene matti = new Inimene("Matti");
-        Inimene john = new Inimene("John");
 
         matti.setPikkus(180);
-        matti.setMass(86);
-
-        john.setPikkus(175);
-        john.setMass(64);
+        matti.setMass(110);
 
         System.out.print(matti.getNimi() + ", kehamassiindeks: ");
         System.out.printf("%5.2f%n", matti.kmi());
-        System.out.print(john.getNimi() + ", kehamassiindeks: ");
-        System.out.printf("%5.2f%n", john.kmi());
+        System.out.println("Matti kaal on: " + matti.getMass());
+
+        matti.nomraalKaal();
+
+        System.out.print(matti.getMass() + "KG | ");
+        System.out.printf("%.2f%n", matti.kmi());
+
     }
 }
