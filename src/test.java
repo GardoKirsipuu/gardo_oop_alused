0b82f50f448b051d619407ef8139bad5565708ee
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2021-02-03 09:24:13 +0200

    Static näide - 03.02

--- src/test.java
+++ src/test.java
@@ -6,7 +6,16 @@ autor Gardo Kirsipuu
 
 public class test {
     public static void main(String[] args) {
-        Inimene2 inim1 = new Inimene2();
+        Auto auto1 = new Auto();
+        auto1.km = 100;
+
+        Auto auto2 = new Auto();
+        auto2.km = 85;
+
+        System.out.println("Auto1 läbitud teekond = " + auto1.km);
+        System.out.println("Auto2 läbitud teekond = " + auto2.km);
+
+        System.out.println(Auto.km);
 
     }
 }
